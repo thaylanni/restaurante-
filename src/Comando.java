da43c881e0bf2ba54dfc8dd50c67177390c79d85
thaylanni <mourathaylann2@gmail.com>
2024-01-15 18:46:16 -0300

    commit

--- src/Comando.java
+++ src/Comando.java
@@ -1,20 +1,4 @@
-
-// Classe para adicionar item ao pedido
-class ComandoAdicionarItem implements Comando {
-    private Pedido pedido;
-    private ItemMenu itemMenu;
-    private int quantidade;
-
-    public ComandoAdicionarItem(Pedido pedido, ItemMenu itemMenu, int quantidade) {
-        this.pedido = pedido;
-        this.itemMenu = itemMenu;
-        this.quantidade = quantidade;
-    }
-
-    @Override
-    public void executar() {
-        pedido.adicionarItem(itemMenu, quantidade);
-    }
+// Interface Comando
+interface Comando {
+    void executar();
 }
-
-
--- src/Interface.java
+++
@@ -1,4 +0,0 @@
-// Interface Comando
-interface Comando {
-    void executar();
-}
